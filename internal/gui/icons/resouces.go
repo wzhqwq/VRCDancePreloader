b06fa0870070e25700cc92394022a725bcbff17e
wzhqwq <wzhii345@outlook.com>
2025-03-23 18:32:41 +0800

    Add mutex for thread-safe access to icon resources

--- internal/gui/icons/resouces.go
+++ internal/gui/icons/resouces.go
@@ -4,16 +4,28 @@ import (
 	"embed"
 	"fyne.io/fyne/v2"
 	"io"
+	"sync"
 )
 
 //go:embed *.png
 var FS embed.FS
 var resources = map[string]fyne.Resource{}
+var resourcesMutex sync.Mutex
 
 func GetIcon(name string) fyne.Resource {
+	resourcesMutex.Lock()
 	if r, ok := resources[name]; ok {
+		resourcesMutex.Unlock()
 		return r
 	}
+	resourcesMutex.Unlock()
+	return loadIcon(name)
+}
+
+func loadIcon(name string) fyne.Resource {
+	resourcesMutex.Lock()
+	defer resourcesMutex.Unlock()
+
 	f, err := FS.Open(name + ".png")
 	if err != nil {
 		return nil
@@ -24,5 +36,6 @@ func GetIcon(name string) fyne.Resource {
 	}
 	r := fyne.NewStaticResource(name, data)
 	resources[name] = r
+
 	return r
 }
