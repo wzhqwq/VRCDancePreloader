2a3bc1df89477f0d9835016408a416b887dce259
wzhqwq <wzhii345@outlook.com>
2025-03-30 16:50:17 +0800

    Refactor custom song management to use a thread-safe structure

--- internal/song/raw_song/custom_song.go
+++ internal/song/raw_song/custom_song.go
@@ -4,11 +4,17 @@ import (
 	"fmt"
 	"github.com/wzhqwq/VRCDancePreloader/internal/third_party_api"
 	"strings"
+	"sync"
 
 	"github.com/wzhqwq/VRCDancePreloader/internal/utils"
 )
 
-var customSongMap = make(map[string]CustomSong)
+type CustomSongs struct {
+	sync.Mutex
+	songs map[string]*CustomSong
+}
+
+var customSongs *CustomSongs
 var uniqueIdIncrement = 0
 
 type CustomSong struct {
@@ -46,22 +52,35 @@ func (cs *CustomSong) MatchUrl(url string) bool {
 	return cs.Url == url
 }
 
-func FindCustomSong(url string) (*CustomSong, bool) {
+func (m *CustomSongs) Find(url string) (*CustomSong, bool) {
+	m.Lock()
+	defer m.Unlock()
+
 	key := url
 	if id, isYoutube := utils.CheckYoutubeURL(url); isYoutube {
 		key = fmt.Sprintf("yt_%s", id)
 	}
-	song, ok := customSongMap[key]
-	return &song, ok
+	song, ok := m.songs[key]
+	return song, ok
 }
 
-func FindOrCreateCustomSong(title, url string) *CustomSong {
-	if song, ok := FindCustomSong(url); ok {
+func (m *CustomSongs) FindOrCreate(title, url string) *CustomSong {
+	m.Lock()
+	defer m.Unlock()
+
+	if song, ok := m.songs[url]; ok {
 		return song
 	}
 	song := NewCustomSong(title, url)
-
-	customSongMap[song.UniqueId] = song
-
+	m.songs[song.Url] = &song
 	return &song
+}
+
+func FindOrCreateCustomSong(title, url string) *CustomSong {
+	if customSongs == nil {
+		customSongs = &CustomSongs{
+			songs: make(map[string]*CustomSong),
+		}
+	}
+	return customSongs.FindOrCreate(title, url)
 }
