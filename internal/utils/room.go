56f60511a9e53ffbb4238b0662b77d5411669391
wzhqwq <wzhii345@outlook.com>
2025-09-08 22:19:54 +0800

    Enhancement: identify DuDuFitDance

--- internal/utils/room.go
+++ internal/utils/room.go
@@ -9,5 +9,8 @@ func IdentifyRoomBrand(roomName string) string {
 	if strings.Contains(roomName, "WannaDance") {
 		return "WannaDance"
 	}
+	if strings.Contains(roomName, "DuDu") && strings.Contains(roomName, "Fit") {
+		return "WannaDance"
+	}
 	return ""
 }
