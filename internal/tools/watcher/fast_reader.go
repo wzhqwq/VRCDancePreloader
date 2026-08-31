c95b60f8ffe5ba6a8928469f4967d5dd9c8d0238
wzhqwq <wzhii345@outlook.com>
2026-08-31 15:43:59 +0800

    Fix: deadlock in reverse log reader

--- internal/tools/watcher/fast_reader.go
+++ internal/tools/watcher/fast_reader.go
@@ -105,19 +105,6 @@ func readReverse(wc int, file *os.File, offset int64) error {
 	rest := make([]byte, 0, bufSize)
 
 	for {
-		lineChan := make(chan Line, 100)
-
-		var wg sync.WaitGroup
-		for i := 0; i < wc; i++ {
-			wg.Add(1)
-			go func() {
-				defer wg.Done()
-				for line := range lineChan {
-					backtraceLine(line.version, line.line)
-				}
-			}()
-		}
-
 		if offset == 0 {
 			return nil
 		}
@@ -135,34 +122,51 @@ func readReverse(wc int, file *os.File, offset int64) error {
 			return err
 		}
 		data := append(buf[:n], rest...)
-		end := len(data)
-		for {
-			idx := bytes.LastIndexByte(data[:end], '\n')
-			if idx < 0 {
-				break
-			}
-			start := idx + 1
-			line := data[start:end]
-			if len(line) > 0 {
-				lineCopy := append([]byte(nil), line...)
-
-				globalVersion--
-				lineChan <- Line{globalVersion, lineCopy}
-			}
-			end = idx
-		}
-		if end > 0 {
-			rest = append(rest[:0], data[:end]...)
-		} else {
-			rest = rest[:0]
-		}
-		close(lineChan)
-
-		wg.Wait()
+		rest = readBlockReverse(wc, data, rest)
 
 		if checkBacktrace() {
 			break
 		}
 	}
 	return nil
+}
+
+func readBlockReverse(wc int, data, rest []byte) []byte {
+	lineChan := make(chan Line, 100)
+	end := len(data)
+
+	var wg sync.WaitGroup
+	for i := 0; i < wc; i++ {
+		wg.Go(func() {
+			for line := range lineChan {
+				backtraceLine(line.version, line.line)
+			}
+		})
+	}
+
+	for {
+		idx := bytes.LastIndexByte(data[:end], '\n')
+		if idx < 0 {
+			break
+		}
+		start := idx + 1
+		line := data[start:end]
+		if len(line) > 0 {
+			lineCopy := append([]byte(nil), line...)
+
+			globalVersion--
+			lineChan <- Line{globalVersion, lineCopy}
+		}
+		end = idx
+	}
+	if end > 0 {
+		rest = append(rest[:0], data[:end]...)
+	} else {
+		rest = rest[:0]
+	}
+	close(lineChan)
+
+	wg.Wait()
+
+	return rest
 }
